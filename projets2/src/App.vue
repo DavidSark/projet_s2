8e5bbcb7f2900d43529a1067dfb5c5bedb003a69
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-05-15 14:47:28 +0200

    prettier & tailwindcss

--- projets2/src/App.vue
+++ projets2/src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
-
-
-<h1>DPNGDZPNGDZNGPN</h1>
+  <h1>test</h1>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -24,7 +28,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
